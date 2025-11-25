d1646f17666fc051d7d51c53412445ac5e60e389
Ihor Pershin <issue.07.08.09@gmail.com>
2025-11-25 18:21:32 +0200

    Delete settings.py, create dir settings with base, dev, prod

---
+++ todo_list_site/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+
+DEBUG = config('DEBUG', default=True, cast=bool)
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "localhost:85",
+    "127.0.0.1",
+    config("SERVER", default="127.0.0.1"),
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
---
+++ todo_list_site/settings/prod.py
@@ -0,0 +1,35 @@
+from .base import *
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    "todo_list.onrender.com",
+    "localhost",
+    "localhost:85",
+    "127.0.0.1",
+    config("SERVER", default="127.0.0.1"),
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": int(os.environ["POSTGRES_DB_PORT"]),
+    }
+}
