775b6a67ec3f076cde43e5019b90baef837f8ddb
Ihor Pershin <issue.07.08.09@gmail.com>
2025-11-25 18:43:42 +0200

    Add Task model

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        URGENT = "Urgent"
+        HIGH = "High"
+        MEDIUM = "Medium"
+        LOW = "Low"
+
+    name = models.CharField(
+        max_length=255,
+        verbose_name="Task_name"
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Task_description"
+    )
+    deadline = models.DateField(
+        verbose_name="Deadline",
+        null=True,
+        blank=True
+    )
+    is_done = models.BooleanField(
+        default=False,
+        verbose_name="Done"
+    )
+    priority = models.CharField(
+        max_length=10,
+        choices=Priority.choices,
+        default=Priority.MEDIUM,
+        verbose_name="Priority"
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="tasks",
+        verbose_name="tags",
+    )
+
+    def __str__(self):
+        return f"{self.name} ({'Is done' if self.is_done else 'In progress'})"
