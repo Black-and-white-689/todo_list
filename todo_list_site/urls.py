4d423d5018adaf7d309ab2ee48e6ee0d7914eb95
Ihor Pershin <issue.07.08.09@gmail.com>
2025-11-25 20:22:17 +0200

    Delete 404 and 500 in urls.py

--- todo_list_site/urls.py
+++ todo_list_site/urls.py
@@ -6,10 +6,6 @@ from django.conf import settings
 
 from django.conf.urls.static import static
 
-from django.conf.urls import handler404, handler500
-
-from django.shortcuts import render
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -20,27 +16,4 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
 
-    def show_500(request):
-        return render(request, "500.html", status=500)
-
-
-    def show_404(request, exception=None):
-        return render(request, "404.html", status=404)
-
-
-    urlpatterns += [path("__debug__/", include(debug_toolbar.urls)),
-                    path("test500/", show_500),
-                    path("test404/", show_404), ]
-
-
-def custom_404(request, exception):
-    return render(request, "404.html", status=404)
-
-
-def custom_500(request):
-    return render(request, "500.html", status=500)
-
-
-handler404 = "arms_drones.urls.custom_404"
-
-handler500 = "arms_drones.urls.custom_500"
+    urlpatterns += [path("__debug__/", include(debug_toolbar.urls)),]
